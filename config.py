84949a25544e1726abce7d0a95eb52baafddf382
twotrees <nick@twotrees.capital>
2023-04-03 09:18:17 +0100

    Moved 'Latest data' div to left hand side. Added fonts to repository. Added font_style variable. Edited styling.

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 summary_folder = '/Users/twotrees/Library/CloudStorage/OneDrive-TwoTreesCapital/portfolio_data/csv/summary'
 test_folder = '/Users/twotrees/Library/CloudStorage/OneDrive-TwoTreesCapital/portfolio_dashboard2'
+font_style1 = "CathaySansLt"
+font_style2 = "CathaySansMd"
